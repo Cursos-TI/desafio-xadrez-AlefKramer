979d18ca5d76e41a3aed977f8d62d93a4087746f
Dev Alef Kramer <66548444+AlefKramer@users.noreply.github.com>
2025-02-13 17:26:32 +0000

    criei a funçao para mov do bispo, diagonal superior direita .

--- xadrez.c
+++ xadrez.c
@@ -4,3 +4,12 @@
 #define BISPO_MOV 5
 #define TORRE_MOV 5
 #define RAINHA_MOV 8
+
+// Função para movimentação do Bispo (diagonal superior direita)
+void moverBispo() {
+    printf("Movimentação do Bispo:\n");
+    for (int i = 1; i <= BISPO_MOV; i++) {
+        printf("Diagonal Superior Direita - Passo %d\n", i);
+    }
+    printf("\n");
+}
